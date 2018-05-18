f2bec02e93b8d6bfa32232df59d778175518ab9c
Alex Lööf <alex.loofet@gmail.com>
2018-05-18 16:57:06 +0200

    libs for auth

--- server/resolvers/Mutation.js
+++ server/resolvers/Mutation.js
@@ -1,6 +1,11 @@
 import data from '../db/index'
 
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
 function writePost(parent, args, ctx, info) {
+  const userId = getUserId(context)
   return data[0]
 }
 
