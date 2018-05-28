891ba8d854d5318a1770d97b93196b4b306615b8
Alex Lööf <alex.loofet@gmail.com>
2018-05-28 23:16:34 +0200

    isAuth function helper

---
+++ client/lib/isAuth.js
@@ -0,0 +1,21 @@
+import cookie from 'cookie'
+
+export default ctx => {
+  const isFromServer = !!ctx.req
+
+  let isAuth = false
+
+  if (isFromServer) {
+    const cookies =
+      ctx.req.headers && ctx.req.headers.cookie
+        ? cookie.parse(ctx.req.headers.cookie, {})
+        : null
+    if (cookies && cookies['next-graphql.sid']) {
+      isAuth = true
+    }
+  } else {
+    if (localStorage.getItem('user')) isAuth = true
+  }
+
+  return isAuth
+}
---
+++ client/lib/withUser.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import { Context } from '../context'
+
+export default function withUser(WrappedComponent) {
+  return class extends React.Component {
+    static async getInitialProps(ctx) {
+      const props = {}
+      console.log('KÖRS?')
+
+      if (WrappedComponent.getInitialProps) {
+        Object.assign(
+          props,
+          (await WrappedComponent.getInitialProps(ctx)) || {}
+        )
+      }
+
+      return props
+    }
+    render() {
+      console.log(this.props)
+      return (
+        <Context.Consumer>
+          {context => {
+            if (context) {
+              return (
+                <WrappedComponent
+                  {...this.props}
+                  user={context.state.user}
+                  setUser={context.actions.setUser}
+                  clearUser={context.actions.clearUser}
+                />
+              )
+            }
+          }}
+        </Context.Consumer>
+      )
+    }
+  }
+}
