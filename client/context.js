f963ef94036130879a4271761fc9511a4a42bdc4
Alex Lööf <alex.loofet@gmail.com>
2018-05-28 23:16:14 +0200

    addded context for user

---
+++ client/context.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export const Context = React.createContext()
+
+class ContextProvider extends Component {
+  state = {
+    user: {
+      _id: '',
+      email: '',
+      name: ''
+    }
+  }
+  componentDidMount() {
+    const user = JSON.parse(localStorage.getItem('user'))
+    this.setState(prevState => ({
+      user: { ...prevState.user, ...user }
+    }))
+  }
+  setUser = user => {
+    this.setState(prevState => ({
+      user: { ...prevState.user, ...user }
+    }))
+  }
+  clearUser = () => {
+    this.setState({
+      user: {
+        _id: '',
+        email: '',
+        name: ''
+      }
+    })
+  }
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          state: this.state,
+          actions: { setUser: this.setUser, clearUser: this.clearUser }
+        }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
+
+export default ContextProvider
